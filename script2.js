472783614269cd95428157a41af756a7e0ab062c
karthikbc <karthikeyan@braincert.com>
2025-03-28 23:43:41 +0530

    new page

--- script2.js
+++ script2.js
@@ -1,23 +1,15 @@
-const btn = document.getElementById('switch-theme');
-const currTheme = localStorage.getItem('theme');
-
-const changeTheme = (theme) => {
-     if (theme === 'dark') {
-          btn.checked = true;
-          document.documentElement.setAttribute('data-theme','dark');
-     } else {
-          btn.checked = false;
-          document.documentElement.setAttribute('data-theme','light');
-     }
-     localStorage.setItem('theme', theme);
-}
-
-if (currTheme === 'dark') {
-     changeTheme(currTheme);
-}
-
-btn.addEventListener('click',(e)=>{
-     const currTheme = localStorage.getItem('theme');
-     const toggleTheme = currTheme === 'dark' ? 'light' : 'dark';
-     changeTheme(toggleTheme);
-})
+document.addEventListener("DOMContentLoaded", function () {
+     const button = document.getElementById("show-more-btn");
+     const content = document.querySelector(".grid-suspenser"); 
+     button.addEventListener("click", function () {
+         content.classList.toggle("revel_project");
+         if (content.classList.contains("revel_project")) {
+             button.innerHTML = 'Show Less <i class="bi bi-arrow-up-short fs-4"></i>';
+             button.classList.add("mt-4");
+         } else {
+             button.innerHTML = 'Show More <i class="bi bi-arrow-down-short fs-4"></i>';
+             button.classList.remove("mt-4");
+         }
+     });
+ });
+ 
